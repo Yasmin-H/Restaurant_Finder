244944782625e046348b12b3d5c25430ac5448f1
Yasmin Haidar <yasmin1635@hotmail.com>
2024-03-04 23:10:11 +0000

    Made URL dynamic based on postcode provided and used the setter to update state to equal the url contained in 'restaurants'

--- src/Components/RestaurantList.js
+++ src/Components/RestaurantList.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Restaurant from './Restaurant';
 
-export default function RestaurantList() {
+export default function RestaurantList({restaurant}) {
+
+    if(!restaurant || restaurant.length === 0) {
+        return <div>No restaurant found.</div>;
+    }
+
+const RestaurantComponents = restaurant.map(restaurant => {
+    return <Restaurant key={restaurant.id} name={restaurant.name} cuisines={restaurant.cuisines} rating={restaurant.rating} address={restaurant.address} />
+})
+
   return (
     <div>
       <p>Hello from RestaurantList</p>
--- src/Containers/RestaurantContainer.js
+++ src/Containers/RestaurantContainer.js
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import RestaurantForm from '../Components/RestaurantForm';
 import RestaurantList from '../Components/RestaurantList';
 
-
 export default function RestaurantContainer() {
+    const [restaurants, setRestaurants] = useState([]);
+    const defaultPostcode = 'EC4M7RF';
 
-const [restaurants, setRestaurants] = useState([]);
-const postcode = 'EC4M7RF';
+    useEffect(() => {
+        fetchRestaurants(defaultPostcode);
+    }, []);
 
-useEffect(() => {
-    const fetchRestaurants = async () => {
-        const response = await fetch("http://localhost:8080/restaurants/LE39RL");
-        const data = await response.json();
-        setRestaurants(data);
-    }
-    fetchRestaurants();
+    const fetchRestaurants = async (postcode) => {
+        try {
+            const response = await fetch(`http://localhost:8080/restaurants/${postcode}`);
+            const data = await response.json();
+            setRestaurants(data.restaurants);
+        } catch (error) {
+            console.error('Error fetching restaurants:', error);
+        }
+    };
 
-}, [])
+    const handleFormSubmit = (postcode) => {
+        fetchRestaurants(postcode);
+    };
 
-  return (
-    <div>
-      <p>Hello from RestaurantContainer</p>
-      <RestaurantForm  />
-      <RestaurantList />
-
-    </div>
-  )
+    return (
+        <div>
+            <p>Hello from RestaurantContainer</p>
+            <RestaurantForm onSubmit={handleFormSubmit} />
+            <RestaurantList restaurants={restaurants} />
+        </div>
+    );
 }
+
